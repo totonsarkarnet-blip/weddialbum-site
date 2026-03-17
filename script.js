ab4498d05be2216a0cf2467b841596deaf033a39
totonsarkarnet-blip <totonsarkar.net@gmail.com>
2026-03-18 01:58:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function openImage(img){
-document.getElementById("imgPopup").style.display="flex";
-document.getElementById("popupImg").src=img.src;
-}
-
-function closeImage(){
-document.getElementById("imgPopup").style.display="none";
-}
